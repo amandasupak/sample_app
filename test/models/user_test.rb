578b34e14b041c31ed3f638e832c339e69ec4499
AmandaSupak <Amandasupak@gmail.com>
2023-12-21 23:21:24 -0600

    verify emails saved downcase

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,7 +6,29 @@ class UserTest < ActiveSupport::TestCase
     @user = User.new(name: "Example User", email: "User@example.com")
   end
 
+  test "should be valid" do
+    assert @user.valid?
+  end
   
+  test "name should be present" do
+    assert @user.name = "    "
+    assert_not @user.valid? 
+  end
+  
+  test "email should be present" do
+    assert @user.email = "    "
+    assert_not @user.valid? 
+  end
+  
+  test "name should not be too long" do
+    assert @user.name = "a" * 51
+    assert_not @user.valid? 
+  end
+  
+  test "email should not be too long" do
+    assert @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid? 
+  end
 
   test "email validation should accept valid emails" do
     valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org 
@@ -22,32 +44,12 @@ class UserTest < ActiveSupport::TestCase
     @user.save
     assert_not duplicate_user.valid?
   end
-end
 
+  test "email addresses should be saved as lowercase" do 
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email 
+  end
 
-
-
-
-test "should be valid" do
-  assert @user.valid?
-end
-
-test "name should be present" do
-  assert @user.name = "    "
-  assert_not @user.valid? 
-end
-
-test "email should be present" do
-  assert @user.email = "    "
-  assert_not @user.valid? 
-end
-
-test "name should not be too long" do
-  assert @user.name = "a" * 51
-  assert_not @user.valid? 
-end
-
-test "email should not be too long" do
-  assert @user.email = "a" * 244 + "@example.com"
-  assert_not @user.valid? 
 end
