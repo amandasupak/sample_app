372a14e02de59a616ad51a6482be035f6376e393
AmandaSupak <Amandasupak@gmail.com>
2023-12-21 23:11:30 -0600

    validating emails for uniqueness

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,29 +6,7 @@ class UserTest < ActiveSupport::TestCase
     @user = User.new(name: "Example User", email: "User@example.com")
   end
 
-  test "should be valid" do
-    assert @user.valid?
-  end
-
-  test "name should be present" do
-    assert @user.name = "    "
-    assert_not @user.valid? 
-  end
-
-  test "email should be present" do
-    assert @user.email = "    "
-    assert_not @user.valid? 
-  end
-
-  test "name should not be too long" do
-    assert @user.name = "a" * 51
-    assert_not @user.valid? 
-  end
-
-  test "email should not be too long" do
-    assert @user.email = "a" * 244 + "@example.com"
-    assert_not @user.valid? 
-  end
+  
 
   test "email validation should accept valid emails" do
     valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org 
@@ -41,11 +19,35 @@ class UserTest < ActiveSupport::TestCase
 
   test "email addresses should be unique" do
     duplicate_user = @user.dup
-    duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
   end
-
 end
 
 
+
+
+
+test "should be valid" do
+  assert @user.valid?
+end
+
+test "name should be present" do
+  assert @user.name = "    "
+  assert_not @user.valid? 
+end
+
+test "email should be present" do
+  assert @user.email = "    "
+  assert_not @user.valid? 
+end
+
+test "name should not be too long" do
+  assert @user.name = "a" * 51
+  assert_not @user.valid? 
+end
+
+test "email should not be too long" do
+  assert @user.email = "a" * 244 + "@example.com"
+  assert_not @user.valid? 
+end
