0ff771ed0ca8570e8cf622a5ebc94c44c09aad61
AmandaSupak <Amandasupak@gmail.com>
2024-01-02 17:40:19 -0600

    add sessions controller logic for login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    @user = User.find_by(email: params[:sessions][:email].downcase)
+    if user && user.authenticate(params[:sessions][:password])
+      #login the user and redirect to user's show page
+    else
+      #create an error message
+      render 'new', status: :unprocessable_entity
+  end
+    
+  def destroy
+  end
 end
