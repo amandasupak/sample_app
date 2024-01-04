9f70bf5e3f0eed078654b898050732f57ac9fd10
AmandaSupak <Amandasupak@gmail.com>
2024-01-03 22:50:35 -0600

    fix login bug typo

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       reset_session
-      login_user
+      login_in user
       redirect_to user
     else
       flash.now[:danger] = 'Invalid Login/Password Combination'
