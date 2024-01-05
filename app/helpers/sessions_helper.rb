ce6deb4f006c85292371263337aa5874ffa3afb6
AmandaSupak <Amandasupak@gmail.com>
2024-01-04 22:53:07 -0600

    save current_user for logins

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,13 @@
 module SessionsHelper
+    # Log in current user
     def login_in(user)
         session[:user_id] = user.id
+    end
+
+    # Returns the current user logged-in (if any)
+    def current_user
+        if session[:user_id]
+            @current_user ||= User.find_by(id: session[:user_id])
+        end
     end
 end
