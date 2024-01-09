b3758302a1d9cc7c63e8df79785f3c378acadce7
AmandaSupak <Amandasupak@gmail.com>
2024-01-09 17:48:37 -0600

    add log out functionality

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -15,4 +15,10 @@ module SessionsHelper
     def logged_in?
         !current_user.nil?
     end
+
+    #logs out current user
+    def log_out
+        reset_session
+        @current_user = nil
+    end
 end
